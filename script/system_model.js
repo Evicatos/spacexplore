6967b16d37cc757521da0d3c3554a9b0570ec43e
Mike Arkosy <evicatos@gmail.com>
2013-05-18 12:22:48 -0700

    adding qunit testing files and system model

---
+++ script/system_model.js
@@ -0,0 +1,14 @@
+function Station() {
+    this.trade = false;
+}
+
+function Planet(habitable) {
+    this.habitable = habitable;
+}
+
+function SystemModel() {
+    this.planets = [];
+    this.space_stations = [];
+    this.phenomena = [];
+}
+
---
+++ test/system_model_test.js
@@ -0,0 +1,4 @@
+test( "Station creation", function() {
+    var station = new Station();
+    strictEqual(station.trade, false);
+});
