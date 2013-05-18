dec19b5de73df5ca4824ec35167cd5c07008aed3
Mike Arkosy <evicatos@gmail.com>
2013-05-18 14:08:57 -0700

    updated system_model a bit

--- script/system_model.js
+++ script/system_model.js
@@ -1,14 +1,21 @@
-function Station() {
-    this.trade = false;
+function Station(tradeable, x, y) {
+    this.trade = tradeable || false;
+    this.coordinates = { "x" : x || 0, "y" : y || 0 };
 }
 
-function Planet(habitable) {
-    this.habitable = habitable;
+function Planet(habitable, x, y) {
+    this.habitable = habitable || false;
+    this.coordinates = { "x" : x || 0, "y" : y || 0 };
+}
+
+function Phenomenon(dangerous, x, y) {
+    this.dangerous = dangerous || true;
+    this.coordinates = { "x" : x || 0, "y" : y || 0 };
 }
 
 function SystemModel() {
+    this.coordinates = [0, 0];
     this.planets = [];
     this.space_stations = [];
     this.phenomena = [];
 }
-
--- test/system_model_test.js
+++ test/system_model_test.js
@@ -1,4 +1,59 @@
-test( "Station creation", function() {
+test( "Station", function() {
     var station = new Station();
     strictEqual(station.trade, false);
+    strictEqual(station.coordinates.x, 0);
+    strictEqual(station.coordinates.y, 0);
+    var tradeable = new Station(true, 1, 2);
+    strictEqual(tradeable.trade, true);
+    strictEqual(tradeable.coordinates.x, 1);
+    strictEqual(tradeable.coordinates.y, 2);
+    var no_coords = new Station(true);
+    strictEqual(no_coords.trade, true);
+    strictEqual(no_coords.coordinates.x, 0);
+    strictEqual(no_coords.coordinates.y, 0);
+});
+
+test( "Station", function() {
+    var station = new Station();
+    strictEqual(station.trade, false);
+    strictEqual(station.coordinates.x, 0);
+    strictEqual(station.coordinates.y, 0);
+    var tradeable = new Station(true, 1, 2);
+    strictEqual(tradeable.trade, true);
+    strictEqual(tradeable.coordinates.x, 1);
+    strictEqual(tradeable.coordinates.y, 2);
+    var no_coords = new Station(true);
+    strictEqual(no_coords.trade, true);
+    strictEqual(no_coords.coordinates.x, 0);
+    strictEqual(no_coords.coordinates.y, 0);
+});
+
+test( "Station", function() {
+    var station = new Station();
+    strictEqual(station.trade, false);
+    strictEqual(station.coordinates.x, 0);
+    strictEqual(station.coordinates.y, 0);
+    var tradeable = new Station(true, 1, 2);
+    strictEqual(tradeable.trade, true);
+    strictEqual(tradeable.coordinates.x, 1);
+    strictEqual(tradeable.coordinates.y, 2);
+    var no_coords = new Station(true);
+    strictEqual(no_coords.trade, true);
+    strictEqual(no_coords.coordinates.x, 0);
+    strictEqual(no_coords.coordinates.y, 0);
+});
+
+test( "Station", function() {
+    var station = new Station();
+    strictEqual(station.trade, false);
+    strictEqual(station.coordinates.x, 0);
+    strictEqual(station.coordinates.y, 0);
+    var tradeable = new Station(true, 1, 2);
+    strictEqual(tradeable.trade, true);
+    strictEqual(tradeable.coordinates.x, 1);
+    strictEqual(tradeable.coordinates.y, 2);
+    var no_coords = new Station(true);
+    strictEqual(no_coords.trade, true);
+    strictEqual(no_coords.coordinates.x, 0);
+    strictEqual(no_coords.coordinates.y, 0);
 });
