4c935bf866f3b2e5206cbe4c681bf9de551f9e1b
Mike Arkosy <evicatos@gmail.com>
2013-05-18 18:00:20 -0700

    refactored code more to make the interface more sane to use when it being in a namespace is taken into account

--- script/system_model.js
+++ script/system_model.js
@@ -1,11 +1,8 @@
 var StarSystem = {
-    SystemEntity : function (entity, x, y) {
-        this.entity = entity || {};
-        this.coordinates = { "x" : x || 0, "y" : y || 0 };
-    },
 
-    Station : function (tradeable) {
+    Station : function (tradeable, x, y) {
         this.trade = tradeable || false;
+        this.coordinates = { "x" : x || 0, "y" : y || 0 };
         this.visit = function(ship) {
             if (ship) {
                 ship.visit_station(this);
@@ -13,8 +10,9 @@ var StarSystem = {
         };
     },
 
-    Planet : function (habitable) {
+    Planet : function (habitable, x, y) {
         this.habitable = habitable || false;
+        this.coordinates = { "x" : x || 0, "y" : y || 0 };
         this.visit = function(ship) {
             if (ship) {
                 ship.visit_planet(this);
@@ -22,8 +20,12 @@ var StarSystem = {
         };
     },
 
-    Phenomenon : function (dangerous) {
+    Phenomenon : function (dangerous, x, y) {
         this.dangerous = dangerous || false;
+        this.coordinates = { "x" : x || 0, "y" : y || 0 };
+        // eventually this won't just call "visit" on the ship
+        // but will have a random chance to throw some kind
+        // of disaster at the ship's crew
         this.visit = function(ship) {
             if (ship) {
                 ship.visit_phenomenon(this);
--- test/system_model_test.js
+++ test/system_model_test.js
@@ -10,22 +10,6 @@ function MockShip(state) {
     };
 }
 
-test( "StarSystem::SystemEntity", function() {
-    var entity = new StarSystem.SystemEntity();
-    strictEqual(entity.coordinates.x, 0, "default x match");
-    strictEqual(entity.coordinates.y, 0, "default y match");
-
-    var stat_entity = new StarSystem.SystemEntity(new StarSystem.Station());
-    strictEqual(stat_entity.coordinates.x, 0, "stat x match");
-    strictEqual(stat_entity.coordinates.y, 0, "stat y match");
-    strictEqual(stat_entity.entity.trade, false, "trade match");
-
-    var plan_entity = new StarSystem.SystemEntity(new StarSystem.Planet(), 1, 2);
-    strictEqual(plan_entity.coordinates.x, 1, "plan x match");
-    strictEqual(plan_entity.coordinates.y, 2, "plan y match");
-    strictEqual(plan_entity.entity.habitable, false, "habit match");
-});
-
 test( "StarSystem::Station", function() {
     var station = new StarSystem.Station();
     strictEqual(station.trade, false, "default trade");
@@ -63,15 +47,15 @@ test( "StarSystem::SystemModel", function() {
 
     var mock_ship = new MockShip(false);
 
-    sys.entities.push(new StarSystem.SystemEntity(new StarSystem.Station()));
-    sys.entities.push(new StarSystem.SystemEntity(new StarSystem.Planet(), 1, 1));
-    sys.entities.push(new StarSystem.SystemEntity(new StarSystem.Phenomenon(), 2, 2));
+    sys.entities.push(new StarSystem.Station());
+    sys.entities.push(new StarSystem.Planet(false, 1, 1));
+    sys.entities.push(new StarSystem.Phenomenon(false, 2, 2));
 
     for (var i = 0; i < sys.entities.length; i++) {
-        sys.entities[i].entity.visit(mock_ship);
+        sys.entities[i].visit(mock_ship);
         strictEqual(sys.entities[i].coordinates.x, i, 
-            "system entity " + i + " x matches");
+            "system coordinates " + i + " x matches");
         strictEqual(sys.entities[i].coordinates.y, i, 
-            "system entity " + i + " y matches");
+            "system coordinates " + i + " y matches");
     }
 });
